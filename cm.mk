9bae7a0e01cbfb3c1fee7f3497e38b8b54889ccc
Jesse Chan <jc@linux.com>
2016-12-26 10:54:00 +0800

    LICENSE: GPL-3.0

--- cm.mk
+++ cm.mk
@@ -1,3 +1,18 @@
+#
+# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
+# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
 # Inherit device configuration
 $(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
 $(call inherit-product, device/samsung/universal8890/device_universal8890.mk)
