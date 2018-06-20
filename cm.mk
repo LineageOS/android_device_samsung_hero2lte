0d1f83d83ad5f7ae77a03608ffa51280962ba223
Tom Powell <tom@powell.io>
2018-06-19 22:40:44 -0700

    License: Apache 2.0.
    This was relicensed via approval from original contributors in email
    form.

--- cm.mk
+++ cm.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
+# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Inherit device configuration
 $(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
 $(call inherit-product, device/samsung/universal8890/device_universal8890.mk)
